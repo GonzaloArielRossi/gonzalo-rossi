e71fa061cacc493fbebeece0675d175404784405
Gonzalo Ariel Rossi <gonzaloarielrossi@gmail.com>
2024-12-15 10:49:05 -0300

    init: config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import perfectionist from 'eslint-plugin-perfectionist';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import reactPlugin from 'eslint-plugin-react';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +14,37 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...pluginQuery.configs['flat/recommended'],
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  {
+    ...perfectionist.configs['recommended-natural'],
+    rules: {
+      'perfectionist/sort-objects': [
+        'error',
+        {
+          customGroups: {
+            id: ['id', '_id'],
+          },
+          destructuredObjects: true,
+          groups: ['id', 'unknown'],
+          ignoreCase: true,
+          ignorePattern: [],
+          newlinesBetween: 'ignore',
+          objectDeclarations: true,
+          order: 'asc',
+          partitionByComment: false,
+          partitionByNewLine: false,
+          specialCharacters: 'keep',
+          styledComponents: true,
+          type: 'alphabetical',
+          useConfigurationIf: {},
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
